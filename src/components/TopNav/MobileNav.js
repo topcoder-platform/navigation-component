47c8337274fb0a5fdfd5023cc8c4f421eb5eb968
gets0ul <gets0ul@gmail.com>
2020-01-16 15:20:51 +0700

    Add support so the logo is linkable to configured URL based on logged in status

--- src/components/TopNav/MobileNav.js
+++ src/components/TopNav/MobileNav.js
@@ -6,7 +6,7 @@ import IconMenu from '../../assets/images/icon-menu.svg'
 import IconMagnifyingGlass from '../../assets/images/magnifying_glass.svg'
 import { config } from 'topcoder-react-utils'
 
-const MobileNav = ({ showLeftMenu, onClickLeftMenu, logo, rightMenu }) => (<div>
+const MobileNav = ({ showLeftMenu, onClickLeftMenu, logo, onClickLogo, rightMenu }) => (<div>
   <div className={styles.mobileNav}>
     <div className={styles.leftMenuContainer}>
       <button className={styles.menuBtn} onClick={onClickLeftMenu}>
@@ -17,7 +17,9 @@ const MobileNav = ({ showLeftMenu, onClickLeftMenu, logo, rightMenu }) => (<div>
         )}
       </button>
     </div>
-    <a href='/'>
+    <a
+      onClick={(e) => onClickLogo(e)}
+      href='/'>
       {logo}
     </a>
     {rightMenu && (
@@ -49,6 +51,7 @@ MobileNav.propTypes = {
   showLeftMenu: PropTypes.bool,
   onClickLeftMenu: PropTypes.func,
   logo: PropTypes.node,
+  onClickLogo: PropTypes.func,
   rightMenu: PropTypes.node
 }
 
--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -2,6 +2,7 @@ import React, { useState, useMemo, useEffect, useLayoutEffect, useCallback } fro
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 import _ from 'lodash'
+import { config } from 'topcoder-react-utils'
 
 import styles from './index.module.scss'
 
@@ -137,8 +138,9 @@ const TopNav = ({
     }, 0)
   }
 
-  const handleClickLogo = () => {
-
+  const handleClickLogo = (e) => {
+    e.preventDefault()
+    window.location = loggedIn ? config.URL.HOME : config.URL.BASE
   }
 
   const expandMenu = (menuId, menu2Id) => {
@@ -435,6 +437,7 @@ const TopNav = ({
         <MobileNav
           showLeftMenu={showLeftMenu}
           logo={logo}
+          onClickLogo={handleClickLogo}
           rightMenu={rightMenu}
           onClickLeftMenu={handleClickLeftMenu}
         />
